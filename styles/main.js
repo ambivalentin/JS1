2b2cb660440afe2661ce27c4b57613ad4a76b67d
valentinwirf@gmail.com <valentinwirf@gmail.com>
2025-03-18 21:17:37 +0100

    Added #1 #2 #3 tasks

---
+++ styles/main.js
@@ -0,0 +1,16 @@
+console.log("Hello World!") 
+
+let firstName = "Valentin";
+let age = 32;
+let favoriteColor = "black";
+
+console.log("My name is ${firstName}. I am ${32} years old and my favorite color is ${favoriteColor}");
+
+
+let dogs = 20;
+let cats = 10;
+
+console.log(dogs - cats);
+console.log(dogs + cats);
+console.log(dogs / cats);
+console.log(dogs * cats);
